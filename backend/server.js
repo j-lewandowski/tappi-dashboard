2958923f42f6df622dbb9b499a987933a389aa0b
j-lewandowski <kubalew.pl@gmail.com>
2022-06-14 16:28:39 +0200

    created user route and controller

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Tappi Dashboard API" });
 });
 
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
